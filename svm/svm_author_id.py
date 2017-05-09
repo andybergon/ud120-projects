de795443a20f62bc4f80714adb24638c7d240fa0
Andrea Bergonzo <abergonzo@hubspot.com>
2017-05-09 19:36:21 +0100

    Lesson 2 Exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+import numpy as np
 
 
 ### features_train and features_test are the features for the training
@@ -19,11 +20,34 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+
+clf = SVC(kernel='rbf', C=10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+score = clf.score(features_test, labels_test)
+
+print('[10]: {}, [26]: {}, [50]: {}'.format(clf.predict(features_test[10]),
+                                            clf.predict(features_test[26]),
+                                            clf.predict(features_test[50])))
+
+counter = 0
+for ft in features_test:
+    ft = np.array(ft).reshape(1,-1)
+    if clf.predict(ft) == 1:
+        counter += 1
+
+print counter
+
+print score
 
 #########################################################
 
