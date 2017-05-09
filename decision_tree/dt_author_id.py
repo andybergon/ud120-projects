5e6437d9a70b5f45e2c3850d89180a7e671f6a26
Andrea Bergonzo <abergonzo@hubspot.com>
2017-05-09 19:36:37 +0100

    Lesson 3 Exercise

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -21,9 +21,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
+print len(features_train[0])
 #########################################################
-### your code goes here ###
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+score = clf.score(features_test, labels_test)
+
+# print('[10]: {}, [26]: {}, [50]: {}'.format(clf.predict(features_test[10]),
+#                                             clf.predict(features_test[26]),
+#                                             clf.predict(features_test[50])))
+
+# counter = 0
+# for ft in features_test:
+#     ft = np.array(ft).reshape(1,-1)
+#     if clf.predict(ft) == 1:
+#         counter += 1
+#
+# print counter
+
+print score
 
 
 #########################################################
