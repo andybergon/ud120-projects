99e47dae7421f60d1681bb09d38723d64cc713dc
Andrea Bergonzo <abergonzo@hubspot.com>
2017-05-09 19:36:12 +0100

    Lesson 1 Exercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,31 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
-#########################################################
-### your code goes here ###
+print(features_train)
 
 
 #########################################################
+from sklearn.naive_bayes import GaussianNB
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+score = clf.score(features_test, labels_test)
+
+print score
+#########################################################
